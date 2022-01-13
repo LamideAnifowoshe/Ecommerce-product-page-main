6479ccbf716d0cf0aa6241497ea50d8a9383ef07
LamideAnifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-01-13 22:32:02 +0100

    update style.css

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("here 2");
+var removeCartItemButtons = document.getElementsByClassName("btn-light");
+console.log(removeCartItemButtons);
+for (var i = 0; i > 3; i + 1) {
+  var button = removeCartItemButtons[i];
+  button.addEventListener("click", function (event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+  });
+}
